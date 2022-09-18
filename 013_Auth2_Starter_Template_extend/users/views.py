e26102e9378afa60a012df594c2a37efbfc9892e
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-09-18 23:24:43 +0200

    Django

---
+++ 013_Auth2_Starter_Template_extend/users/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login
+from django.contrib import messages
+from .forms import UserForm, UserProfileForm
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'Logged Out!!')
+    return redirect('home')
+
+
+def register(request):
+    form_user = UserForm()
+    form_profile = UserProfileForm()
+
+    if request.method == 'POST':
+        form_user = UserForm(request.POST)
+        form_profile = UserProfileForm(request.POST, request.FILES)
+
+        if form_user.is_valid() and form_profile.is_valid():
+            user = form_user.save()
+            print(user)
+            profile = form_profile.save(commit=False)
+            profile.user = user
+            profile.save()
+            login(request, user)
+
+            messages.success(request, 'Registered Successfully!!')
+            return redirect('home')
+
+    context = {
+        'form_user': form_user,
+        'form_profile': form_profile
+    }
+    return render(request, 'users/register.html', context)
+
+
+def user_login(request):
+    form = AuthenticationForm(request, request.POST or None)
+
+    if form.is_valid():
+        user = form.get_user()
+        if user:
+            login(request, user)
+            messages.success(request, 'Logged in !!')
+            return redirect('home')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'users/user_login.html', context)
